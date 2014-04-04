67ba0b294cafbbc505f23f43aa3fd617ab1006ec
Barbie <barbie@missbarbell.co.uk>
2014-04-04 07:36:14 +0100

    upped version

--- lib/CPAN/Testers/WWW/Reports/Query/AJAX.pm
+++ lib/CPAN/Testers/WWW/Reports/Query/AJAX.pm
@@ -3,7 +3,7 @@ package CPAN::Testers::WWW::Reports::Query::AJAX;
 use strict;
 use warnings;
 
-our $VERSION = '0.07';
+our $VERSION = '0.08';
  
 #----------------------------------------------------------------------------
 
